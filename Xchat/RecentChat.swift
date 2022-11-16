1b99131f21d69e465ccbb82799e047b478ca0056
Anthony Petrov <beavean@gmail.com>
2022-11-16 19:02:04 +0200

    add RecentChat struct

---
+++ Xchat/RecentChat.swift
@@ -0,0 +1,24 @@
+//
+//  RecentChat.swift
+//  Xchat
+//
+//  Created by Beavean on 16.11.2022.
+//
+
+import Foundation
+import FirebaseFirestoreSwift
+
+struct RecentChat: Codable {
+    
+    var id = ""
+    var chatRoomId = ""
+    var senderId = ""
+    var senderName = ""
+    var receiverId = ""
+    var receiverName = ""
+    var memberIds = [String]()
+    var lastMessage = ""
+    var unreadCounter = 0
+    var avatarLink = ""
+    @ServerTimestamp var date = Date()
+}
