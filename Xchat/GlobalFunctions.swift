55988ece17087b79bb9a0a0095a31eb1fdf4efea
Anthony Petrov <beavean@gmail.com>
2022-11-17 13:12:39 +0200

    add date extension & recent message time elapsed calculation method

--- Xchat/GlobalFunctions.swift
+++ Xchat/GlobalFunctions.swift
@@ -11,3 +11,22 @@ func fileNameFrom(fileUrl: String) -> String {
     guard let name = ((fileUrl.components(separatedBy: "_").last)?.components(separatedBy: "?").first)?.components(separatedBy: ".").first else { return "" }
     return name
 }
+
+func timeElapsed(_ date: Date) -> String {
+    let seconds = Date().timeIntervalSince(date)
+    var elapsed = ""
+    if seconds < 60 {
+        elapsed = "Just now"
+    } else if seconds < 60 * 60 {
+        let minutes = Int(seconds / 60)
+        let minutesText = minutes > 1 ? "mins" : "min"
+        elapsed = "\(minutes) " + minutesText
+    } else if seconds < 24 * 60 * 60 {
+        let hours = Int(seconds / (60 * 60))
+        let hourText = hours > 1 ? "hours" : "hour"
+        elapsed = "\(hours) " + hourText
+    } else {
+        elapsed = date.longDate()
+    }
+    return elapsed
+}
