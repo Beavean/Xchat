0d09ea7e068b315996a7acb818f0ca63d82907bf
Anthony Petrov <beavean@gmail.com>
2022-11-26 14:46:14 +0200

    add configuration to ChatViewController: MessagesDataSource & MessagesLayoutDelegate

--- Xchat/MessagesDataSource.swift
+++ Xchat/MessagesDataSource.swift
@@ -21,4 +21,41 @@ extension ChatViewController: MessagesDataSource {
     func numberOfSections(in messagesCollectionView: MessageKit.MessagesCollectionView) -> Int {
         mkMessages.count
     }
+    
+    //MARK: - Cell top labels
+    
+    func cellTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        if indexPath.section % 3 == 0 {
+            let showLoadMore = false
+            let text = showLoadMore ? "Pull to load more" : MessageKitDateFormatter.shared.string(from: message.sentDate)
+            let font = showLoadMore ? UIFont.systemFont(ofSize: 13) : UIFont.boldSystemFont(ofSize: 10)
+            let color = showLoadMore ? UIColor.systemBlue : UIColor.darkGray
+            return NSAttributedString(string: text, attributes: [.font: font, .foregroundColor: color])
+        }
+        return nil
+    }
+    
+    //MARK: - Cell bottom label
+    
+    func cellBottomLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        if isFromCurrentSender(message: message) {
+            let message = mkMessages[indexPath.section]
+            let status = indexPath.section == mkMessages.count - 1 ? message.status + " " + message.readDate.time() : String()
+            let font = UIFont.boldSystemFont(ofSize: 10)
+            let color = UIColor.darkGray
+            return NSAttributedString(string: status, attributes: [.font: font, .foregroundColor: color])
+        }
+        return nil
+    }
+    
+    //MARK: - Message bottom label
+    
+    func messageBottomLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        if indexPath.section != mkMessages.count - 1 {
+            let font = UIFont.boldSystemFont(ofSize: 10)
+            let color = UIColor.darkGray
+            return NSAttributedString(string: message.sentDate.time(), attributes: [.font: font, .foregroundColor: color])
+        }
+        return nil
+    }
 }
